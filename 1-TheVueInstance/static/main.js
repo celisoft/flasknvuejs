ca144a32d644db8a02dcd39946105203b7092d2d
Celine / Celisoft <celine.liberal@mail.com>
2019-01-22 22:18:19 +0100

    Tuto 01 from VueMastery

---
+++ 1-TheVueInstance/static/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        firstName: "Céline",
+        lastName: "Libéral",
+        message: "Ceci est un super message !"
+    }
+});
